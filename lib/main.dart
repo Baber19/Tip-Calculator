7c87a3d23d19f18e4b120cc70317799b539847e0
Baber19 <Baber19i@outlook.com>
2026-01-13 20:45:03 +0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TipCalculator(),
     );
   }
-
 }
 
 class TipCalculator extends StatelessWidget {
@@ -21,13 +20,178 @@ class TipCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/images/hat_icon.png")),
+          Padding(
+            padding: const EdgeInsets.only(top: 38.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/hat_icon.png"),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Mr\nCalculator",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                        Text(
+                          "Tip",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Text("     Calculator",textAlign: TextAlign.center,)
+                  ],
+                ),
+              ],
             ),
-          )
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 210,
+            width: 280,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              // border: Border.all(width: 1),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white10,
+                  blurRadius: 2,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "Total p/person",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("\$"),
+                    Text("000", style: TextStyle(fontSize: 60)),
+                  ],
+                ),
+                Container(height: 1, width: 240, color: Colors.grey),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text("Total Bill"),
+                          Text(
+                            "\$000",
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text("Total Tip"),
+                          Text(
+                            "\$000",
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 30,),
+          Padding(
+            padding: const EdgeInsets.only(left: 40.0,right: 40),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Enter",style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("your bill")
+                  ],
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: TextField(
+                    
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: Icon(Icons.attach_money),
+                      border: InputBorder.none
+
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+          ),
+          SizedBox(height: 30,),
+          Padding(
+            padding: const EdgeInsets.only(left: 40.0,right: 40),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Choose",style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("your tip")
+                  ],
+                ),
+                SizedBox(width: 20,),
+                Container(
+                  height: 40,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                )
+              ],
+            ),
+          ),
+
         ],
       ),
     );
